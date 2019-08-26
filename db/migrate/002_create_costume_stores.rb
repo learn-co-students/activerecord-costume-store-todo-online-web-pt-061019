2ea8aa35906859bf0d088adf81ec91b401492fed
scrabill <shannon.crabill@gmail.com>
2019-08-26 14:13:49 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,24 @@
 # Create your costume_stores migration here
+# Create a class and inherit from ActiveRecord::Migration
+
+# By convention, the class name should match the part of the
+# file name after the number, so in this case:
+# 002_create_costumes.rb becomes class CreateCostumes
+
+# Define a change method in which to do the migration
+# In this change method, create columns with the correct names and
+# value types according to the spec
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |table|
+      table.string :name
+      table.string :location
+      table.integer :costume_inventory
+      table.integer :num_of_employees
+      table.boolean :still_in_business
+      table.timestamp :opening_time
+      table.timestamp :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |table|
+      table.string :name
+      table.string :location
+      table.string :theme
+      table.integer :price
+      table.boolean :family_friendly
+      table.timestamp :opening_date
+      table.timestamp :closing_date
+      table.text :description
+    end
+  end
+end
