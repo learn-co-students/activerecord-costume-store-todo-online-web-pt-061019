643ec48edc03679a98f679ee753560770de03153
fasnimo <fasnimo@gmail.com>
2019-09-26 23:32:50 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create your costume_stores migration here
-class CreateCostumeStores < ActiveRecord:: Migration[5.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
     def change 
         creat_table :costume_stores do |t|
             t.string :name
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord:: Migration[5.2]
             t.integer :num_of_employees
             t.boolean :still_in_business
             t.datetime :opening_time
-            t:datetime :closing_time
+            t.datetime :closing_time
         end
     end
 end
